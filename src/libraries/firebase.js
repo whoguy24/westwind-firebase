6016b3bb92dd32a6191a4f2a9cfe408f384e9ded
whoguy24 <whoguy24@gmail.com>
2023-03-06 14:13:34 -0600

    Wrote test code to pull data from firestore, to prove or disprove that it will work with GoDaddy shared hosting.

--- src/libraries/firebase.js
+++ src/libraries/firebase.js
@@ -1,23 +1,18 @@
-import firebase from 'firebase/app'
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAfj1HaGNJ3r783qI_MgGehWa8qjsxknI",
-  authDomain: "westwind-morgans-377918.firebaseapp.com",
-  databaseURL: "https://westwind-morgans-377918-default-rtdb.firebaseio.com",
-  projectId: "westwind-morgans-377918",
-  storageBucket: "westwind-morgans-377918.appspot.com",
-  messagingSenderId: "597094908498",
-  appId: "1:597094908498:web:88dd373472f95e35e0d58b",
-  measurementId: "G-E14QW04W7R"
-};
+    apiKey: "AIzaSyDAfj1HaGNJ3r783qI_MgGehWa8qjsxknI",
+    authDomain: "westwind-morgans-377918.firebaseapp.com",
+    databaseURL: "https://westwind-morgans-377918-default-rtdb.firebaseio.com",
+    projectId: "westwind-morgans-377918",
+    storageBucket: "westwind-morgans-377918.appspot.com",
+    messagingSenderId: "597094908498",
+    appId: "1:597094908498:web:88dd373472f95e35e0d58b",
+    measurementId: "G-E14QW04W7R"
+  };
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
-}
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-export const storage = firebase.storage();
+  export default db;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import styles from '@/styles/Home.module.css'
 
+import Test from "../components/Test.jsx"
+
 export default function Home() {
   return (
     <>
       <main>
         <h1>WELCOME HOME</h1>
+        <Test />
       </main>
     </>
   )
