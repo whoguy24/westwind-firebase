50ee2ba33845f7d406fd0d22a2bc08f928874e57
whoguy24 <whoguy24@gmail.com>
2023-03-07 13:09:25 -0600

    Began construction of Navigation and Template components.

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////
+///// COMPONENT MODULES ///////////////////////////////
+///////////////////////////////////////////////////////
+
+// Import React Libraries
+
+// Import MUI Components
+
+// Import Components
+
+///////////////////////////////////////////////////////
+///// COMPONENT DESCRIPTION ///////////////////////////
+///////////////////////////////////////////////////////
+
+// The Navigation component renders the navigation bar on the top of the website.
+// ----------------------------------------------------
+// It is used to navigate to multiple pages in the web application. There are two primary modes: 
+// Large: used for larger screens, like desktops and larger tablets.
+// Small: used for smaller screens, like mobile devices and smaller tablets.
+// ----------------------------------------------------
+// Mode will be determined based on viewport size, available to the component via the useContext() hook.
+// Navigation bar is intended to be globally available across the web application at all times.
+// ----------------------------------------------------
+// Used in:
+// pages/_app.js
+
+///////////////////////////////////////////////////////
+///// COMPONENT FUNCTION //////////////////////////////
+///////////////////////////////////////////////////////
+
+export default function Navigation() {
+    
+    return (
+        <>
+        </>
+    );
+    
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
+import Navigation from "@/components/Navigation";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>  
+      <Navigation />
+      <Component {...pageProps} />
+    </>
+  )
 }
