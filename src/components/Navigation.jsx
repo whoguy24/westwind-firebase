fc41f3202ba0fc7e090a954c2b0c2cc6cceb6c85
whoguy24 <whoguy24@gmail.com>
2023-03-08 12:06:22 -0600

    Experimenting with different layout styles in Home component.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -96,6 +96,7 @@ export default function Navigation() {
                     </Link>
 
                 </div>
+
             </AppBar>
 
         </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
+// Import Styles
+import styles from '@/styles/home.module.css'
+
 export default function Home() {
   return (
     <>
-      <main>
-      </main>
+      <div id={ styles.home }>
+      </div>
     </>
   )
 }
