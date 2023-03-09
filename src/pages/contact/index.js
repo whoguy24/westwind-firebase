2a2aa994b4cde5fec2359e818fb30e347ebe8e83
whoguy24 <whoguy24@gmail.com>
2023-03-09 11:18:46 -0600

    Testing different methods to ensure responsiveness to banner images in the contact page.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,9 @@
 export default function Contact() {
     return (
       <>
+        <div id="banner">
+          <img id="banner-image" src={`/assets/banner_contact.png`}/>
+        </div>
       </>
     )
   }
